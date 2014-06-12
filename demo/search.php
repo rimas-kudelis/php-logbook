b66a3da8fb6f8517a55f470f384c87e4e011d864
Rimas Kudelis <rq@akl.lt>
2014-06-12 12:48:55 +0300

    Added search form into search.php; made showing search results conditional.

--- demo/search.php
+++ demo/search.php
@@ -108,99 +108,126 @@ NN4 does not understand @import
 		die ("Error " . $e->getCode() . " : " . $e->getMessage());
 	}
 
+	echo "\n<table border=\"0\" cellpadding=\"10\" cellspacing=\"10\">"
+		. "\n<tr>"
+		. "\n\t<td bgcolor=\"#CCCCFF\">"
+		. "\n\t<h2>Full Log Search</h2>"
+		. "\n\t<p>Search the log book<br>"
+		. "\n\t<form action=\"search.php\" method=\"POST\">"
+		. "\n\t<p>"
+		. "\n\tEnter a callsign: <input type=\"text\" name=\"callsign\">"
+		. "\n\t<p>"
+		. "\n\t<input type=\"submit\" value=\"Search Log!\">"
+		. "\n\t</form>"
+		. "\n\t</td>"
+		. "\n</tr>"
+		. "\n</table>";
+
 	// Read the callsign to search.
-	$callsign = $_POST['callsign'];
-	
-	// Make the callsign upper case
-	$callsign = strtoupper ($callsign);
-
-
-	echo "<CENTER><H1>Log Search result for $callsign</H1></CENTER>";
-	echo "<P>";
-	
-	// Fetch the list of DX callsigns available
-	$dxcalls = $logBook->getDXCallsigns();
-
-	// Create an array of the bands to be displayed. This is hard coded to make my life easier. It should
-	// really be read from the database and the HTML table dynamically created. Let as a later exercise....
-	$bands = array (160,80,40,30,20,17,15,12,10);
-
-	// Initialise the 2-d array i.e. set number of QSOs on each band for each DX station to zero. This
-	// make populating the HTML table a little easier.
-	for ($i=0; $i < count($dxcalls); $i++)
-		for ($j=0; $j < count($bands); $j++)
-			$table[$dxcalls[$i]][$bands[$j]] = 0;
-
-	// Query the database for all the QSOs for the requested callsign
-	$qsos = $logBook->searchQSOs($callsign);
-	if (empty($qsos))
+	if (isset($_POST['callsign']))
 	{
-		echo "<P>Sorry no QSOs found for $callsign!<P>";
-		$count = 0;
-	}
-	else
-	{
-		// Table Headings - bands
-		echo "\n<center><table BORDER=\"1\" CELLSPACING=\"0\" CELLPADDING=\"5\" width=\"70%\">\n<tr>\n" .
-			"\n\t<th>Callsign</th>";
-		foreach ($bands as $band)
+		$callsign = strtoupper($_POST['callsign']);
+
+		echo "<CENTER><H1>Log Search result for $callsign</H1></CENTER>";
+		echo "<P>";
+
+		// Fetch the list of DX callsigns available
+		$dxcalls = $logBook->getDXCallsigns();
+
+		// Create an array of the bands to be displayed. This is hard coded to make my life easier. It should
+		// really be read from the database and the HTML table dynamically created. Let as a later exercise....
+		$bands = array (160,80,40,30,20,17,15,12,10);
+
+		// Initialise the 2-d array i.e. set number of QSOs on each band for each DX station to zero. This
+		// make populating the HTML table a little easier.
+		for ($i=0; $i < count($dxcalls); $i++)
+			for ($j=0; $j < count($bands); $j++)
+				$table[$dxcalls[$i]][$bands[$j]] = 0;
+
+		// Query the database for all the QSOs for the requested callsign
+		$qsos = $logBook->searchQSOs($callsign);
+		if (empty($qsos))
 		{
-			echo "\n\t<th>{$band}</th>";
+			echo "<P>Sorry no QSOs found for $callsign!<P>";
+			$count = 0;
 		}
-		echo "\n</tr>" .
-			"\n<p>";
-
-		foreach ($qsos as $row)
+		else
 		{
-			// Add up the number of QSOs on each band
-			switch ($row["op_mode"])
+			// Table Headings - bands
+			echo "\n<center><table BORDER=\"1\" CELLSPACING=\"0\" CELLPADDING=\"5\" width=\"70%\">\n<tr>\n" .
+				"\n\t<th>Callsign</th>";
+			foreach ($bands as $band)
 			{
-				case 'SSB':
-					$table [$row["dxcallsign"]] [$row["band"]] += 1;
-					break;
-
-				case 'CW':
-					$table [$row["dxcallsign"]] [$row["band"]] += 2;
-					break;
-
-				case 'DIG':
-					$table [$row["dxcallsign"]] [$row["band"]] += 4;
-					break;
-
+				echo "\n\t<th>{$band}</th>";
 			}
-		}
-		$count = count($qsos);
+			echo "\n</tr>" .
+				"\n<p>";
 
-		// We have now read all the QSOs made for all the DX stations into a 2D matrix ($table)
-		// Now we go through each row (DX station) and column (band)
-
-		foreach ($table as $k => $v)
-		{
-			// Count the number of QSOs made with this DX station
-			$total = array_reduce ($v, 'add_up');
-
-			// None? Then don't bother displaying the row
-			if ($total == 0)
-				continue;
-
-			echo "\n<tr>";
-			echo "\n\t<td width=\"10%\"><center>$k</center></td>";
-
-			foreach ($v as $k2 => $v2)
+			foreach ($qsos as $row)
 			{
-				// Display QSOs made on each band
-				if(in_array($k2, $bands))
+				// Add up the number of QSOs on each band
+				switch ($row["op_mode"])
 				{
-					print_cell($v2);
+					case 'SSB':
+						$table [$row["dxcallsign"]] [$row["band"]] += 1;
+						break;
+
+					case 'CW':
+						$table [$row["dxcallsign"]] [$row["band"]] += 2;
+						break;
+
+					case 'DIG':
+						$table [$row["dxcallsign"]] [$row["band"]] += 4;
+						break;
+
 				}
 			}
-			echo "\n</tr>";
-		}
+			$count = count($qsos);
 
-		echo "\n</table></center>";
-		echo "\n\n";
-	
-	} // End else no QSOs found
+			// We have now read all the QSOs made for all the DX stations into a 2D matrix ($table)
+			// Now we go through each row (DX station) and column (band)
+
+			foreach ($table as $k => $v)
+			{
+				// Count the number of QSOs made with this DX station
+				$total = array_reduce ($v, 'add_up');
+
+				// None? Then don't bother displaying the row
+				if ($total == 0)
+					continue;
+
+				echo "\n<tr>";
+				echo "\n\t<td width=\"10%\"><center>$k</center></td>";
+
+				foreach ($v as $k2 => $v2)
+				{
+					// Display QSOs made on each band
+					if(in_array($k2, $bands))
+					{
+						print_cell($v2);
+					}
+				}
+				echo "\n</tr>";
+			}
+
+			echo "\n</table></center>";
+			echo "\n\n";
+		} // End else no QSOs found
+
+		switch ($count)
+		{
+			case 1:
+				echo "\n<CENTER><P><P><B>Total of $count QSO with $callsign</B><P>";
+				break;
+
+			case 0:
+				break;
+
+			default:
+				echo "\n<CENTER><P><P><B>Total of $count QSOs with $callsign</B><P>";
+				break;
+		}
+	}
 
 	// Record the end time of the script
 	$end = microtime();
@@ -216,23 +243,9 @@ NN4 does not understand @import
 	// Count the number of QSOs in the database
 	$totalQSOCount = $logBook->getQSOCount();
 			
-	switch ($count)
-	{
-		case 1:
-			echo "\n<CENTER><P><P><B>Total of $count QSO with $callsign</B><P>";
-			break;
-			
-		case 0:
-			break;
-			
-		default:
-			echo "\n<CENTER><P><P><B>Total of $count QSOs with $callsign</B><P>";
-			break;
-	}
-	
 	echo "\n<P>There are $totalQSOCount QSOs in the Database<P>";
 
-	echo "\n<P><FONT SIZE=\"-2\">The search took $elapsedTime seconds</FONT></CENTER>";
+	echo "\n<P><FONT SIZE=\"-2\">Page load took $elapsedTime seconds</FONT></CENTER>";
 	
 ?>
 
